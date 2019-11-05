69b2e3f7460747c966f19a6cc1df4c0e71827008
Lucas Robitaille <lrjames237@gmail.com>
2019-11-05 16:49:36 -0600

    added initial fetch request

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route } from 'react-router-dom'
 import './App.css'
 import LandingPage from './Components/LandingPage/LandingPage'
@@ -7,18 +7,52 @@ import LoginForm from './Components/LoginForm/LoginForm'
 import Dashboard from './Components/Dashboard/Dashboard'
 import Collection from './Components/CollectionPage/CollectionPage'
 import TeaPage from './Components/TeaPage/TeaPage'
+import config from './config'
 
-function App() {
-  return (
-    <main className='App'>
-      <Route exact path='/' component={LandingPage}/>
-      <Route path='/signup' component={SignUpForm}/>
-      <Route path='/login' component={LoginForm} />
-      <Route path='/dashboard' component={Dashboard} />
-      <Route path='/collection/ripe' component={Collection}/>
-      <Route path='/tea/dark-depths' component={TeaPage}/>
-    </main>
-  );
+class App extends Component {
+  state = {
+    users: [],
+    collections: [],
+    teas: [],
+    sessions: []
+  }
+
+  componentDidMount() {
+    Promise.all([
+        fetch(`${config.API_ENDPOINT}/collection`),
+        fetch(`${config.API_ENDPOINT}/tea`),
+        fetch(`${config.API_ENDPOINT}/session`)
+    ])
+        .then(([collectionRes, teaRes, sessionRes]) => {
+            if (!collectionRes.ok)
+                return collectionRes.json().then(e => Promise.reject(e));
+            if (!teaRes.ok)
+                return teaRes.json().then(e => Promise.reject(e));
+            if (!sessionRes.ok)
+                return sessionRes.json().then(e => Promise.reject(e));
+
+            return Promise.all([collectionRes.json(), teaRes.json(), sessionRes.json()]);
+        })
+        .then(([collections, teas, sessions]) => {
+            this.setState({collections, teas, sessions});
+        })
+        .catch(error => {
+            console.error({error});
+        });
+ }
+
+  render(){
+    return (
+      <main className='App'>
+        <Route exact path='/' component={LandingPage}/>
+        <Route path='/signup' component={SignUpForm}/>
+        <Route path='/login' component={LoginForm} />
+        <Route path='/dashboard' component={Dashboard} />
+        <Route path='/collection/ripe' component={Collection}/>
+        <Route path='/tea/dark-depths' component={TeaPage}/>
+      </main>
+    );
+  }
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    PORT: process.env.PORT || 8080,
+    API_ENDPOINT: "http://localhost:8000/api"
+  };
+  
