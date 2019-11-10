47d34694020be1408c9ccb7203a4491ac86995fe
Lucas Robitaille <lrjames237@gmail.com>
2019-11-10 14:04:30 -0600

    added configuration

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    PORT: process.env.PORT || 8080,
-    API_ENDPOINT: process.env.REACT_APP_API_BASE_URL 
-  };
+  PORT: process.env.PORT || 8080,
+  API_ENDPOINT: process.env.REACT_APP_API_BASE_URL || "http://localhost:8000/api"
+};
+
   
